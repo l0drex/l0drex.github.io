54dcab6435c37f710126bde9b282216ce2483182
l0drex <l0drex@pm.me>
2025-01-04 12:43:29 +0100

    Add 404 page

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://l0drex.github.io/",
-  integrations: [icon(), tailwind()]
+  integrations: [icon(), tailwind(), robotsTxt({
+    sitemap: false,
+    policy: [{ userAgent: "*", disallow: ["/404"] }],
+  })]
 });
