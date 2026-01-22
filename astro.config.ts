f7809366af5ab3f9bb4f33b206b0ad6b04ad9027
l0drex <l0drex@pm.me>
2026-01-22 15:36:19 +0100

    Update tailwindcss

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://l0drex.github.io/",
-  integrations: [icon(), tailwind(), robotsTxt({
-    sitemap: false,
-    policy: [{ userAgent: "*", disallow: ["/404"] }],
-  })]
+
+  integrations: [
+    icon(),
+    robotsTxt({
+      sitemap: false,
+      policy: [{ userAgent: "*", disallow: ["/404"] }],
+    }),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
