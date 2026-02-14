77d01ec748b0a8b69190122b92422d840cceb329
l0drex <l0drex@pm.me>
2026-02-14 10:56:06 +0100

    Improve overall style
    Add border to project items
    Adjust button border and background
    Capitalize project type
    etc

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -4,3 +4,15 @@ export function dateFormatter(date: Date, short: boolean = false) {
     year: "numeric",
   });
 }
+
+// Source - https://stackoverflow.com/a/1026087
+// Posted by Steve Harrison, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-02-14, License - CC BY-SA 4.0
+
+export function capitalizeFirstLetter(text: string) {
+  if (!text) {
+    return text;
+  }
+
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
