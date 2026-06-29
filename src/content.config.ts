f94427cad8de0074a3f1cfba5aace9f5b3d2bb12
l0drex <l0drex@pm.me>
2026-06-29 12:29:49 +0200

    Update astrojs

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,6 @@
-import { defineCollection, reference, z } from "astro:content";
+import { defineCollection, reference } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
 import { ProjectType } from "@utils/projects";
 
 const blogBase = {
@@ -7,26 +9,26 @@ const blogBase = {
 };
 
 const projectCollection = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/*.md", base: "./src/content/projects" }),
   schema: ({ image }) =>
     z.object({
       ...blogBase,
       description: z.string(),
-      type: z.nativeEnum(ProjectType),
+      type: z.enum(ProjectType),
       color: z.optional(z.string()),
       colorDark: z.optional(z.string()),
       languages: z.array(z.string()),
       tools: z.optional(z.array(z.string())),
-      sourceCode: z.optional(z.string().url()),
-      url: z.optional(z.string().url()),
-      external: z.optional(z.string().url()),
+      sourceCode: z.optional(z.url()),
+      url: z.optional(z.url()),
+      external: z.optional(z.url()),
       small: z.optional(z.boolean()).default(true),
       image: z.optional(image()),
     }),
 });
 
 const blogCollection = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
   schema: z.object({
     ...blogBase,
     project: z.optional(reference("projects")),
